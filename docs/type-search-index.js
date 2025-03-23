f3a2a09292683030692fbc4cf04bab0168cfcadd
DarkMortal <dragonbeast.saiyan@gmail.com>
2025-03-23 05:47:48 +0530

    Updated Documentation

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"complex","l":"Complex"},{"p":"complex","l":"ComplexInterface"},{"p":"evaluator","l":"Evaluator"},{"p":"evaluator","l":"ExpressionEvaluator"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"complex","l":"Complex"},{"p":"complex","l":"ComplexInterface"},{"p":"evaluator","l":"Evaluator"},{"p":"evaluator","l":"ExpressionEvaluator"}];updateSearchResults();
